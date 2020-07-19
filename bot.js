67dd664b732811acee9338a2c8e44f6ccd1448de
selino12 <68498287+selino12@users.noreply.github.com>
2020-07-19 02:41:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['734303795076530206'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
